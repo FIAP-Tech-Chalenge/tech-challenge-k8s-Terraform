8c42b97376d41410c5c6c67b67a92c3e2ac443a1
wolwerr <ricardo@dtmm.com.br>
2024-04-15 10:41:25 -0300

    "Enviando arquivos de configuração do terraform"

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "labRole"{
+    default = "arn:aws:iam::559588913990:role/LabRole"
+}
+
+variable "subnetA"{
+    default = "subnet-0a4f3a7a1762c42a4"
+}
+
+variable "subnetB"{
+    default = "subnet-096152ce70558c695"
+}
+
+variable "subnetC"{
+    default = "	subnet-0d621425c9fb661fd"
+}
+
+variable "sgId"{
+    default = "sg-076557bdea12448e8"
+}
+
+variable "accessConfig"{
+    default = "API_AND_CONFIG_MAP"
+}

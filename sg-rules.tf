a7e0d237727abced349a2b9ce37ae2d5f2cedf6e
wolwerr <ricardo@dtmm.com.br>
2024-05-01 22:45:16 -0300

    "Acertando banco de dados para RDS e liberando portas para conexão"

---
+++ sg-rules.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group_rule" "ingress_rule" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "sg-0fcda7794cde48e6c"
+}

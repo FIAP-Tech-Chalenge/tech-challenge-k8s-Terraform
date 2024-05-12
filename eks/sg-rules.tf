009933949e2a2434d22ffea6a043031b269d1eb8
wolwerr <ricardo@dtmm.com.br>
2024-05-12 20:01:56 -0300

    "Acertando o sg-rules para pegar o security_group"

--- eks/sg-rules.tf
+++ eks/sg-rules.tf
@@ -4,5 +4,5 @@ resource "aws_security_group_rule" "ingress_rule" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "sg-0fcda7794cde48e6c"
+  security_group_id = aws_security_group.sg.id
 }

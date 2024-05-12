04bb14e98b56a6a99c5229e82a32d6201aa1ac24
wolwerr <ricardo@dtmm.com.br>
2024-05-11 23:39:24 -0300

    Alterando lb

--- lb/lb-listener.tf
+++ lb/lb-listener.tf
@@ -1,3 +1,12 @@
+resource "aws_alb" "alb" {
+  name               = "ALB-${var.projectName}"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [var.subnetA, var.subnetB, var.subnetC]
+  security_groups    = [aws_security_group.sg.id]
+  idle_timeout       = 60
+}
+
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.alb.arn
   port              = "8080"

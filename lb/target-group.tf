72afb82d93ce056cb7928f1d97520936a22383bf
wolwerr <ricardo@dtmm.com.br>
2024-05-11 23:00:20 -0300

    "Criando um terraform-destroy"

--- lb/target-group.tf
+++ lb/target-group.tf
@@ -13,5 +13,10 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+resource "aws_lb_target_group_attachment" "attach" {
+    target_group_arn = aws_lb_target_group.tg.arn
+    target_id        = data.aws_instance.ec2.id
+    port             = 30000
+}
 
 

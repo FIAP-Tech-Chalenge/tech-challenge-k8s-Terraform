761db59ba431f111753df06fbe2e1119edc896a7
wolwerr <ricardo@dtmm.com.br>
2024-05-11 23:19:01 -0300

    "Acertando alb"

--- eks/alb.tf
+++ eks/alb.tf
@@ -7,14 +7,3 @@ resource "aws_alb" "alb" {
   idle_timeout       = 60
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_alb.alb.arn
-  port              = "8080"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-
-}

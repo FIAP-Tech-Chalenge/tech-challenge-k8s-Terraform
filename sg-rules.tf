6edc8238550682cebf72e7a1e91bda8f2ccbd779
wolwerr <ricardo@dtmm.com.br>
2024-05-13 16:44:00 -0300

    "Acertando ingress"

--- sg-rules.tf
+++ sg-rules.tf
@@ -1,4 +1,14 @@
 data "aws_security_group" "existing_sg" {
+
+  depends_on = [
+    aws_lb_target_group.tg,
+    aws_eks_cluster.eks-cluster,
+    aws_eks_node_group.node-group,
+    aws_alb.alb,
+    aws_lb_listener.listener,
+    aws_security_group.sg
+  ]
+
   filter {
     name   = "group-name"
     values = ["eks-cluster-sg-tech-3555990"]

4046236f0936f31cbb9bdebd759a60c369b2439d
wolwerr <ricardo@dtmm.com.br>
2024-05-12 23:51:30 -0300

    "Acertando a chamada tg_arn"

--- lb/lbTG.tf
+++ lb/lbTG.tf
@@ -1,10 +1,5 @@
-variable "tg_arn" {
-    description = "The ARN of the target group"
-    type        = string
-}
-
 resource "aws_lb_target_group_attachment" "attach" {
-    target_group_arn = var.tg_arn
+    target_group_arn = "TG-tech"
     target_id        = data.aws_instance.ec2.id
     port             = 30000
 }

e0e46afb9ccd42d8057b98c0cec1cb9dc10fe346
wolwerr <ricardo@dtmm.com.br>
2024-05-13 00:31:50 -0300

    "Teste para o eks-cluster, liberando as portas"

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -11,3 +11,12 @@ resource "aws_eks_cluster" "eks-cluster" {
     authentication_mode = var.accessConfig
   }
 }
+
+resource "aws_security_group_rule" "ingress_rule" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+}
--- eks/sg-rules.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group_rule" "ingress_rule" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sg.id
-}

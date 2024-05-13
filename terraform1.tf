c1d4043594b8eff7ea0bcf78921d10fd70527e6e
wolwerr <ricardo@dtmm.com.br>
2024-05-13 14:54:09 -0300

    "Colocando dependencia entre a crianção"

--- terraform1.tf
+++ terraform1.tf
@@ -110,8 +110,6 @@ resource "aws_lb_listener" "listener" {
 
 }
 
-
-
 resource "aws_lb_target_group" "tg" {
   name        = "TG-${var.projectName}"
   port        = 30000
@@ -128,6 +126,15 @@ resource "aws_lb_target_group" "tg" {
 }
 
 data "aws_instance" "ec2" {
+  depends_on = [
+    aws_lb_target_group.tg,
+    aws_eks_cluster.eks-cluster,
+    aws_eks_node_group.node-group,
+    aws_alb.alb,
+    aws_lb_listener.listener,
+    aws_security_group.sg
+  ]
+
   filter {
     name = "tag:eks:nodegroup-name"
     values = ["NG-tech"]
@@ -135,7 +142,15 @@ data "aws_instance" "ec2" {
 }
 
 resource "aws_lb_target_group_attachment" "attach" {
-  depends_on = [aws_lb_target_group.tg, data.aws_instance.ec2]
+  depends_on = [
+    aws_lb_target_group.tg,
+    data.aws_instance.ec2,
+    aws_eks_cluster.eks-cluster,
+    aws_eks_node_group.node-group,
+    aws_alb.alb,
+    aws_lb_listener.listener,
+    aws_security_group.sg
+  ]
 
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
